9ec7075376f8ff1a655c6780942e4ea76894fd10
ekusiadadus <ekusiadadus@gmail.com>
2022-02-01 06:46:18 +0900

    ADD: Email, Password Fields

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 # 2022.02.1
 
 ![2022.02.1](docs/images/instagram_1.png)
+![2022.02.1](docs/images/instagram_2.png)
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learn/utils/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_learn/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key? key}) : super(key: key);
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Flexible(child: Container(), flex: 2),
               // svg image
               SvgPicture.asset(
                 'assets/ic_instagram.svg',
@@ -28,7 +40,18 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 64),
               // text field input for email
+              TextFieldInput(
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+              ),
               // text field input for password
+              TextFieldInput(
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
+              ),
               // button login
               // Transitioning to signin up
             ],
