912c8ffc2ebbfdfea918fd2959c8de66d0740dad
ekusiadadus <ekusiadadus@gmail.com>
2022-02-03 00:14:09 +0900

    Add: Poast Page

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/utils/colors.dart';
 
 class AddPostScreen extends StatefulWidget {
   AddPostScreen({Key? key}) : super(key: key);
@@ -10,10 +11,36 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: IconButton(
-      icon: const Icon(Icons.upload),
-      onPressed: () {},
-    ));
+    // return Center(
+    //   child: IconButton(
+    //     icon: const Icon(Icons.upload),
+    //     onPressed: () {},
+    //   ),
+    // );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroudColor,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: const Text('Post to '),
+        centerTitle: false,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Post',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
